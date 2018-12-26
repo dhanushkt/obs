eac60a8f3d6b3b2f5f1040e72d562576573d7b1a
Dhanush KT <dhanushkt787@gmail.com>
2018-12-26 23:45:35 +0530

    db updated

---
+++ db/obs__26-12-2018_DNS.sql
@@ -0,0 +1,184 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 26, 2018 at 07:14 PM
+-- Server version: 10.1.29-MariaDB
+-- PHP Version: 7.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `obs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `aid` int(10) NOT NULL,
+  `aname` varchar(50) NOT NULL,
+  `aemail` varchar(50) NOT NULL,
+  `amob` bigint(11) NOT NULL,
+  `apassword` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bills`
+--
+
+CREATE TABLE `bills` (
+  `bill_id` int(100) NOT NULL,
+  `billdate` varchar(50) DEFAULT NULL,
+  `custname` varchar(100) DEFAULT NULL,
+  `custemail` varchar(100) DEFAULT NULL,
+  `custphno` bigint(12) DEFAULT NULL,
+  `custmethod` varchar(50) NOT NULL,
+  `cno` int(10) NOT NULL,
+  `discount` double(20,2) NOT NULL DEFAULT '0.00',
+  `amt_paid` double(20,2) NOT NULL DEFAULT '0.00',
+  `total_amt` double(20,2) NOT NULL DEFAULT '0.00',
+  `amt_due` double(20,2) NOT NULL DEFAULT '0.00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bill_items`
+--
+
+CREATE TABLE `bill_items` (
+  `item_id` int(250) NOT NULL,
+  `ibill_id` int(100) NOT NULL,
+  `item_name` varchar(200) NOT NULL,
+  `item_desc` mediumtext NOT NULL,
+  `iunit_cost` decimal(10,2) NOT NULL,
+  `iquantity` int(100) NOT NULL,
+  `itax` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE `clients` (
+  `cno` int(10) NOT NULL,
+  `cuname` varchar(50) NOT NULL,
+  `cemail` varchar(50) NOT NULL,
+  `cphno` bigint(11) NOT NULL,
+  `cpassword` varchar(100) NOT NULL,
+  `sname` varchar(50) NOT NULL,
+  `semail` varchar(50) NOT NULL,
+  `sphno` varchar(50) NOT NULL,
+  `saddress` mediumtext NOT NULL,
+  `sgstno` bigint(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reset_password`
+--
+
+CREATE TABLE `reset_password` (
+  `email` varchar(200) NOT NULL,
+  `tempstr` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`aid`),
+  ADD UNIQUE KEY `aname` (`aname`),
+  ADD UNIQUE KEY `aemail` (`aemail`);
+
+--
+-- Indexes for table `bills`
+--
+ALTER TABLE `bills`
+  ADD PRIMARY KEY (`bill_id`),
+  ADD KEY `shop_info` (`cno`);
+
+--
+-- Indexes for table `bill_items`
+--
+ALTER TABLE `bill_items`
+  ADD PRIMARY KEY (`item_id`),
+  ADD KEY `ibill_id` (`ibill_id`);
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`cno`);
+
+--
+-- Indexes for table `reset_password`
+--
+ALTER TABLE `reset_password`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `aid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `bill_items`
+--
+ALTER TABLE `bill_items`
+  MODIFY `item_id` int(250) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `cno` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bills`
+--
+ALTER TABLE `bills`
+  ADD CONSTRAINT `shop_info` FOREIGN KEY (`cno`) REFERENCES `clients` (`cno`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `bill_items`
+--
+ALTER TABLE `bill_items`
+  ADD CONSTRAINT `bills` FOREIGN KEY (`ibill_id`) REFERENCES `bills` (`bill_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
