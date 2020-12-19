9a358f9ce98143dc029e5252c16e307e86907d15
dhanushkt <dhanushkt787@gmail.com>
2020-12-19 12:17:51 +0530

    favicons and header title changes

--- client/assets/csslink.php
+++ client/assets/csslink.php
@@ -1,6 +1,6 @@
 <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="../plugins/images/favicon.png">
-    <title>AlphaSystems - OBS</title>
+    <title>Invoice | Infinity Systems</title>
     <!-- Bootstrap Core CSS -->
     <link href="../plugins/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="../plugins/bower_components/bootstrap-extension/css/bootstrap-extension.css" rel="stylesheet">
