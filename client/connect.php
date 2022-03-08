d0b6072ddb0f74cc620a5e636f7aa000dcaeb1af
dhanushkt <dhanushkt787@gmail.com>
2022-03-08 10:39:10 +0530

    universal connection changes

--- client/connect.php
+++ client/connect.php
@@ -1,16 +1,19 @@
 <?php
-$connection = mysqli_connect("103.102.234.230","kersahtn_invoiceadmin","invoiceadmin@12");
-if(!$connection)
-{
-	echo "failed to connect";
-	//die("Database Connection Failed" . mysqli_error($connection));
-}
-$dbselect = mysqli_select_db($connection,'kersahtn_invoice');
-if(!$dbselect)
-{
-	echo "failed to select the database";
-	//die("failed to select the database" . mysqli_error($connection)); 
-	//die function is used to stop the executon of the page and (.) is used for concatinain since its a sting output
-}
+
+include_once('./login/connect.php');
+
+// $connection = mysqli_connect("103.102.234.230","kersahtn_invoiceadmin","invoiceadmin@12");
+// if(!$connection)
+// {
+// 	echo "failed to connect";
+// 	//die("Database Connection Failed" . mysqli_error($connection));
+// }
+// $dbselect = mysqli_select_db($connection,'kersahtn_invoice');
+// if(!$dbselect)
+// {
+// 	echo "failed to select the database";
+// 	//die("failed to select the database" . mysqli_error($connection)); 
+// 	//die function is used to stop the executon of the page and (.) is used for concatinain since its a sting output
+// }
 
 ?>
