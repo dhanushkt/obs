14a72c14ca1e7cc8ff27c8463cf9f0b97ba56f47
dhanushkt <dhanushkt787@gmail.com>
2022-03-08 14:35:28 +0530

    reverted back global connection changes

--- client/connect.php
+++ client/connect.php
@@ -1,19 +1,19 @@
 <?php
 
-include_once('./login/connect.php');
+//include_once('./login/connect.php');
 
-// $connection = mysqli_connect("103.102.234.230","kersahtn_invoiceadmin","invoiceadmin@12");
-// if(!$connection)
-// {
-// 	echo "failed to connect";
-// 	//die("Database Connection Failed" . mysqli_error($connection));
-// }
-// $dbselect = mysqli_select_db($connection,'kersahtn_invoice');
-// if(!$dbselect)
-// {
-// 	echo "failed to select the database";
-// 	//die("failed to select the database" . mysqli_error($connection)); 
-// 	//die function is used to stop the executon of the page and (.) is used for concatinain since its a sting output
-// }
+$connection = mysqli_connect("103.108.220.200","kersahtn_invoiceadmin","invoiceadmin@12");
+if(!$connection)
+{
+	echo "failed to connect";
+	//die("Database Connection Failed" . mysqli_error($connection));
+}
+$dbselect = mysqli_select_db($connection,'kersahtn_invoice');
+if(!$dbselect)
+{
+	echo "failed to select the database";
+	//die("failed to select the database" . mysqli_error($connection)); 
+	//die function is used to stop the executon of the page and (.) is used for concatinain since its a sting output
+}
 
 ?>
